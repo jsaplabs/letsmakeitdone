dbcc39a9e7e643c88842a39fcd948dd3b653ba49
Amantha Jayathilake <amanthajayathilake@gmail.com>
2021-12-29 21:16:25 +0530

    changes

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -5,8 +5,7 @@ export const Services = (props) => {
         <div className='section-title'>
           <h2>Our Services</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
-            dapibus leonec.
+          We provide you with excellent services to support and cater your requirements.
           </p>
         </div>
         <div className='row'>
